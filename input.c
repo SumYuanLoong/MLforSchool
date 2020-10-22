a8df98bf9b1a72ef5ba60e0695a91520712a89a5
steees <kosoonteck@gmail.com>
2020-10-23 00:52:04 +0800

    updated with multi-array

--- input.c
+++ input.c
@@ -11,21 +11,30 @@ int main()
 float readfile()
 {
     float mydatanumber; //naming for dataset float number
-    float arraydata[1000]; //array for dataset float number
-    int i = 0;
+    const int row = 100;
+    const int col = 10;
+    float arraydata[row][col]; //array for dataset float number
+    int a1 = 0;
+    int a2 = 0;
     FILE *mydata=fopen(".\\data_1-4.txt", "r"); //import dataset.txt file
     
-    //print out all the dataset float number excluding comma seperator and store into array
-    for (i == 0; i <= 1000; i++)
+    if (mydata == NULL)//file checker
     {
-        fscanf(mydata, "%f, ", &mydatanumber); //get content from dataset.txt file and ignore comma
-        arraydata[i] = mydatanumber; //store dataset float number into array
-        
-        //printing output for easy reference, can be removed in future.
-        printf("array data: %.2f", arraydata[i]);
-        printf("\narray data count: %d:\t", i);
-        printf("%.2f \n", mydatanumber);
+        printf("The file cannot be found. %s", mydata); 
     }
-    
+    else
+    {
+        //print out all the dataset float number excluding comma seperator and store into array
+        for (a1 = 0; a1 < row; a1++)
+        {
+            for (a2 = 0; a2 < col; a2++)
+            {
+                fscanf(mydata, "%f, ", &mydatanumber); //get content from dataset.txt file and ignore comma
+                arraydata[a1][a2] = mydatanumber; //store dataset float number into array
+                //printing output for easy reference, can be removed in future.
+                printf("Patient: %d, Data input: %d, array data: %.2f \n", a1 + 1, a2 + 1, arraydata[a1][a2]);
+            }
+        }
     fclose(mydata); //close imported file and exit.
+    }
 }
