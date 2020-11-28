b32038399ec7296fc6530178d1ee5f831537e6ba
Sum Yuan Loong/HomeP-PC <sumyuanloong@gmail.com>
2020-11-28 15:52:11 +0800

    changes to sigmoid and init back propagate

--- multi-neuro.c
+++ multi-neuro.c
@@ -20,8 +20,8 @@ float TestSetDiag[tsRow]; //testing result set
 
 float trainz[neurons][trRow]; //store training set z value of each patient
 float testz[neurons][tsRow]; //store testing set z value of each patient
-//float trainsig[trRow]; //store training set sigmoid y cap of each patient
-//float testsig[tsRow]; //store testing set sigmoid y cap of each patient
+float trainsig[neurons][trRow]; //store training set sigmoid y cap of each patient
+float testsig[neurons][tsRow]; //store testing set sigmoid y cap of each patient
 float neuTrainZ[trRow],neuTrainSig[trRow]; //Z value and y cap value for the central neuron
 float neuTestZ[tsRow],neuTestSig[tsRow]; //Z value and y cap value for the central neuron
 
@@ -51,9 +51,10 @@ void readFile();                //remain unchanged
 /* flag 1 for training, 0 for test*/
 void linearRegress(short flag);
 void neuronRegress(short flag);
-void sigmoid(double inArr[], double outArr[],int arrSz);
+float sigmoid(double z);
 void mmseFunc(double *trainMmse,double *testMmse);
 double maeFunc();
+void neuBackPropagate(); // back propagation for the central neuron
 void backPropagate();
 double random();
 void matrix();
@@ -74,16 +75,31 @@ int main(){
             printf("weight[%d]:%lf\n", y, weight[x][y]);
         }
     }
+
+    //COLLECTION OF ORIGINAL DATA RUN1
     linearRegress(1);
     linearRegress(0);
     neuronRegress(1);
     neuronRegress(0);
-    sigmoid(neuTrainZ,neuTrainSig,trRow);
-    sigmoid(neuTestZ,neuTestSig,tsRow);
     mmseFunc(putrmmse, putsmmse);
     printf("utrmmse = %f, utsmmse = %f",utrmmse,utsmmse);
+    //COLLECTION OF INITIAL DATA COMPLETE
 
-    printf("test");
+
+    FILE * temp = fopen("data.temp", "w");
+    int iteration = 1;
+    double mae = maeFunc();
+    while (mae > TMAE)
+    {
+        fprintf(temp, "%d %f\n", iteration, mae);
+        neuBackPropagate();
+        linearRegress(1);
+        iteration++;
+        mae = maeFunc();
+    }
+
+    fclose(temp);
+
     
 
     return 0;
@@ -93,17 +109,19 @@ void linearRegress(short flag){
     //TO check if run through of entire dataset is needed
     int maxRows;
     float *pdataset, *reset;
-    float *pzArr;
+    float *pzArr, *pSigArr;
     //assignment of pointers by choice
     if(flag == 1){
         maxRows = sizeof(TrainSetData)/sizeof(*pTrainSetData);
         pdataset = pTrainSetData;     
         pzArr = ptrainz;
+        pSigArr = &trainsig[0][0];
     }
     else if(flag == 0){
         maxRows = sizeof(TestSetData)/sizeof(*pTestSetData);
         pdataset = pTestSetData;    
         pzArr = ptestz;    
+        pSigArr = &testsig[0][0];
     }
     reset = pdataset;
 
@@ -116,7 +134,9 @@ void linearRegress(short flag){
             {
                 z += (weight[n][b] * *pdataset) + bias[n];
                 *pzArr = z;
+                *pSigArr = sigmoid(z);
                 pzArr++; // increment to next value in z arrary
+                pSigArr++;
                 b = 0;
                 z = 0; //reset column and z row values
             }
@@ -133,16 +153,18 @@ void linearRegress(short flag){
 void neuronRegress(short flag){
     int maxRows;
     float *pdataset;
-    float *pzArr;
+    float *pzArr,*pSigArr;
     if(flag == 1){
-        maxRows = sizeof(trainz)/sizeof(*ptrainz);
-        pdataset = ptrainz;     
+        maxRows = sizeof(trainsig)/sizeof(trainsig[0][0]);
+        pdataset = &trainsig[0][0];     
         pzArr = pneuTrainZ;
+        pSigArr = &neuTrainSig[0];
     }
     else{
-        maxRows = sizeof(testz)/sizeof(*ptestz);
-        pdataset = ptestz;    
+        maxRows = sizeof(testsig)/sizeof(testsig[0][0]);
+        pdataset = &testsig[0][0];    
         pzArr = pneuTestZ;    
+        pSigArr = &neuTestSig[0];
     }
 
     int a,b,c = 0; // x is loop counter, y for position in row, c for row
@@ -150,8 +172,8 @@ void neuronRegress(short flag){
     for(a=0, b=0; a< maxRows; a++,pdataset++){
         if(b == (neurons-1)){
             z += (neuWeight[b] * *pdataset) + neuBias;
-            //printf("nuez%d : %f\n",a,z);
             *pzArr = z;
+            *pSigArr = sigmoid(z);
             pzArr++;   // increment to next value in z arrary
             b=0;
             z=0;        //reset column and z row values
@@ -163,12 +185,10 @@ void neuronRegress(short flag){
     }
 }
 
-void sigmoid(double inArr[], double outArr[],int arrSz){
-    int i;
-    for(i=0;i<arrSz;i++){
-        outArr[i] = (1/(1+exp(-inArr[i])));
-        printf("");
-    }
+float sigmoid(double z){
+
+    return (1/(1+exp(-z)));
+
 }
 
 void mmseFunc(double *trainMmse,double *testMmse){
@@ -186,6 +206,38 @@ void mmseFunc(double *trainMmse,double *testMmse){
     *testMmse = mmsesum/tsRow;
 }
 
+double maeFunc(){
+    int i;
+    double maesum = 0;
+    for(i=0; i<trRow;i++){
+        maesum += fabs(neuTrainSig[i] - TrainSetDiag[i]);
+    }
+    return maesum/90;
+}
+
+void neuBackPropagate()
+{
+    int x, y, n;
+    double sumtrainw = 0, sumtrainb = 0;
+    for (y = 0; y < neurons; y++)
+    {
+        for (x = 0; x < trRow; x++)
+        {
+            sumtrainw += ((neuTrainSig[x] - TrainSetDiag[x]) * (exp(neuTrainZ[x]) / ((1 + exp(neuTrainZ[x])) * (1 + exp(neuTrainZ[x])))) * trainsig[y][x]);
+
+            if (y == 8)
+            {
+                sumtrainb += ((neuTrainSig[x] - TrainSetDiag[x]) * (exp(neuTrainZ[x]) / ((1 + exp(neuTestZ[x])) * (1 + exp(neuTestZ[x])))) * 1);
+            }
+        }
+        sumtrainw = (sumtrainw / trRow);  //saved and reused in hidden layer old weight is also saved and reused
+        neuWeight[y] = (neuWeight[y] - (trainspeed * sumtrainw)); //update the new weight into oldw[0-8]
+
+        sumtrainw = 0;
+    }
+    sumtrainb = (sumtrainb / trRow);
+    neuBias = (neuBias - (trainspeed * sumtrainb)); //update the new bias b into oldb
+}
 
 
 void readFile(){
